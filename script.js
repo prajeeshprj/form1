170f54c9baaec8fe23e9d4dfab87fbbd9f9e88c2
prajeeshprj <prajeeshprasanna@gmail.com>
2022-03-14 18:17:48 +0530

    commit form

---
+++ script.js
@@ -0,0 +1,59 @@
+window.addEventListener("load",main);
+
+function _(id){
+    return document.getElementById(id);
+}
+function main(){
+    _("signup-form").addEventListener("submit",validate);
+}
+function validate(event){
+    event.preventDefault();
+    var formData = new FormData(event.target)
+    var name = formData.get("Username")
+    var email = formData.get("mail id")
+    var password = formData.get("Password")
+    var confirmPassword = formData.get("confirmpassword")
+    var phone = formData.get("Phonenumber")
+    nameValidation(name,"name-error");
+    emailValidation(email,"mail-id-error");
+}
+    // if(!Username && Username<10)
+    // _("username-error").innerHTML = "Please enter username"
+    // if(!mail id)
+    // _("mail id-error").innerHTML = "Please enter valid mail id"
+    // if(phoneNumber<10)
+    // _("phonenmber-error").innerHTML = "Please enter valid mail id"
+    
+
+    function nameValidation(value,id){
+        isEmptyOrShort(value, id , 3, "Name");
+    }
+
+    function isEmptyOrShort(value, id, minlength, key){
+        if(!value){
+            setError(id, "Please enter your"+key);
+            return;
+        }
+        if(value.length < minlength){
+            setError(id, key + "must be atleast" + minlength + "characters");
+        return;
+        }
+        setError(id, "");
+
+    }
+
+    function emailValidation(value, id){
+        if(!value){
+            setError(id,"please enter your email");
+            return;
+        }
+        if (!value.includes("@")) {
+            setError(id, "Please enter a valid email");
+            return;
+        }
+        setError(id, "");
+    }
+
+    function setError(id, message){
+        _(id).innerHTML = message;
+    }
